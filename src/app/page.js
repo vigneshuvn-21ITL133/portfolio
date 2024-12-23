67e463463afb6e0ba234186c335b2a80d8208866
V.N VIGNESHU <126391500+vigneshuvn-21ITL133@users.noreply.github.com>
2024-12-23 12:33:38 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
-import Index from './Index'
-
-
+import Aboutme from './pages/About';
+import Project from './pages/project';
+import Tech from './pages/Tech';
+import Contact from './pages/contact';
+import Footer from './pages/Footer';
 
 function page() {
   return (
-    <Index/>
+    <>
+    <Aboutme/>
+    <Project/>
+    <Tech/>
+    <Contact/>
+    <Footer/>
+    </>
   );
 }
 export default page;
